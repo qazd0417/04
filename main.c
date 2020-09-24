a9e0cbf11c867dd3ae766fb7f28f724298ef41df
qazd0417 <70630107+qazd0417@users.noreply.github.com>
2020-09-24 16:44:22 +0900

    ysj04_06

--- main.c
+++ main.c
@@ -6,11 +6,10 @@
 int main(int argc, char *argv[]) {
 	int a;
 	
-	printf("input the year:");
+	printf("input the second:");
 	scanf("%i", &a);
 	
-	printf("is %i leaf year? %i\n", a,
-									(a%4==0 && a%100!=0)||(a%400==0));
+	printf("The time for %i second is %i:%i:%i\n", a, a/3600, (a%3600)/60, a%60);
 	
 	return 0;
 }
